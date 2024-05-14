8add11652d02a0ff07e45c5e06f64344fc3e4459
Sanktah <13367222+Sanktah@users.noreply.github.com>
2024-05-13 23:10:08 -0400

    feat: resolve hack 10

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,12 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    #...
-    return result  
+    result2 = []
+    result = result.replace('a','@')
+    result = result.replace('o','0')
+    result = result.replace('i','1')
+    for i in result:
+        i = i.upper()
+        result2.append(i)
+    result = result2
+    return result
