7e1979c9d74c9ba055f02d28f5de42c6df7e91a5
Sanktah <13367222+Sanktah@users.noreply.github.com>
2024-05-13 22:57:32 -0400

    feat: resolve hack 9

--- hack_9.py
+++ hack_9.py
@@ -4,5 +4,8 @@ loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
 
 def fn_hack_9():
     result = [1,2,3]
-    #...
+    contador = 0
+    while contador < len(result):
+        result.insert(contador + 1, "@")
+        contador += 2
     return result  
